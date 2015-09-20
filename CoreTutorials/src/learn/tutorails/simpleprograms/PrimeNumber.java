9bd121ba3ce821446a033ff2733c325de3407332
Saurabh Kedia <kedia.saurabh@gmail.com>
2015-09-21 00:31:49 +0530

    Sample2

---
+++ CoreTutorials/src/learn/tutorails/simpleprograms/PrimeNumber.java
@@ -0,0 +1,27 @@
+package learn.tutorails.simpleprograms;
+
+public class PrimeNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int input = 89;
+		boolean result = isInputPrime(input);
+		System.out.println("The number " + input + " is prime " + result);
+	}
+
+	private static boolean isInputPrime(int input) {
+		int count = 0;
+		for(int i = 1; i <= input/2; i++){
+			if(input%i == 0){
+				count++;
+			}
+		}
+		if(count == 1){
+			return true;
+		} else 
+			return false;
+	}
+
+}

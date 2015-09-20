81dada3216f2d50e1f9889d6e7e333d22decb2e0
Saurabh Kedia <kedia.saurabh@gmail.com>
2015-09-21 00:48:08 +0530

    Sample3

---
+++ CoreTutorials/src/learn/tutorails/simpleprograms/Palindrome.java
@@ -0,0 +1,26 @@
+package learn.tutorails.simpleprograms;
+
+public class Palindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int input = 12321;
+		String inputString = String.valueOf(input);
+		char[] inputArray = inputString.toCharArray();
+		char[] resultArray = new char[inputArray.length];
+		String resultString = "";
+		for(int i = 0; i < inputArray.length; i++){
+			resultString += inputArray[inputArray.length - 1 - i];
+			resultArray[i] = inputArray[inputArray.length - 1 - i];
+		}
+		System.out.println(resultString);
+		int result = Integer.parseInt(resultString);
+		if(result == input){
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+}

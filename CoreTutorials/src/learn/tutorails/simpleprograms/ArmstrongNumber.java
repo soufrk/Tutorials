663a2546e944b0d9dbdaafa1582bf2b8313b8527
Saurabh Kedia <kedia.saurabh@gmail.com>
2015-09-22 21:56:28 +0530

    Sample 4

---
+++ CoreTutorials/src/learn/tutorails/simpleprograms/ArmstrongNumber.java
@@ -0,0 +1,26 @@
+package learn.tutorails.simpleprograms;
+
+public class ArmstrongNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int input = 375;
+		int dupinput = input;
+		int armstrongNo = 0;
+		while(input != 0){ 
+			int temp = 0;
+			temp = input%10;
+			temp = temp*temp*temp;
+			armstrongNo += temp;
+			input = input/10;
+		}
+		if(armstrongNo == dupinput){
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+}

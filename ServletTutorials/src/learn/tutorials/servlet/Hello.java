808b560715c53afd264c943b21197a459bbecde9
Saurabh Kedia <kedia.saurabh@gmail.com>
2015-10-26 00:14:47 +0530

    Servlet Tutorials - Lesson 1

---
+++ ServletTutorials/src/learn/tutorials/servlet/Hello.java
@@ -0,0 +1,51 @@
+package learn.tutorials.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//set content type
+		response.setContentType("text/html");
+		//get the response writer object
+		PrintWriter out = response.getWriter();
+		// get the parameter sent in the request url named "user"
+		String name = request.getParameter("user");
+		out.print("<html><body>");
+		out.print("<b> Hello Servlet - Get Method by " + name + "<b>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

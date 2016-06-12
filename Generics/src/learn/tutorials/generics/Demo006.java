c7a07dfa27f9e80058754286c48539fcf8cf7353
Saurabh Kedia <kedia.saurabh@gmail.com>
2016-06-12 22:47:41 +0530

    Generics, Inheritance, and Subtype

--- Generics/src/learn/tutorials/generics/Demo006.java
+++ Generics/src/learn/tutorials/generics/Demo006.java
@@ -5,9 +5,10 @@ package learn.tutorials.generics;
  * <b>Generics, Inheritance, and Subtypes</b>
  * <li>On providing upper bound to the type argument we can pass its sub-types
  * as well</li>
- * <li>For ex :- T extends Number then T will accept numbers and its subclasses<br/>
+ * <li>For ex :- T extends Number then T will accept numbers and its subclasses
+ * <br/>
  * public <T extends Number> void print(T t)</li>
- * <li>But the case like Box<Number> box = new Box<>() will not accept </li>
+ * <li>But the case like Box<Number> box = new Box<>() will not accept</li>
  * </ul>
  * 
  * @author saurabh.kedia
@@ -16,8 +17,17 @@ package learn.tutorials.generics;
 public class Demo006 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		printObject(1);
+		printObject(1.0);
+//		Below statement will give compilation error even though integer is subclass of Number
+		//Subtype<Number> subType = new Subtype<Integer>();
 	}
 
+	public static <T extends Number> void printObject(T t) {
+		System.out.println(t.toString());
+	}
+}
+
+class Subtype<T> {
+	T t;
 }

acd0fdbc0276a62c6197ea944c7a76eb228433b9
Saurabh Kedia <kedia.saurabh@gmail.com>
2016-07-31 14:26:00 +0530

    Updated Comments

--- DynamicProgramming/src/practice/technique/dynamic/programming/PalindromePartition.java
+++ DynamicProgramming/src/practice/technique/dynamic/programming/PalindromePartition.java
@@ -4,6 +4,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+/**
+ * Find all the substring palindromes of the given string
+ * 
+ * @author KD
+ *
+ */
 public class PalindromePartition {
 
 	public static void main(String[] args) {
@@ -12,20 +18,27 @@ public class PalindromePartition {
 		System.out.println("Raw String Set ::: " + strSet);
 		Iterator<String> strSetIterator = strSet.iterator();
 
-		while(strSetIterator.hasNext()){
+		while (strSetIterator.hasNext()) {
 			String current = strSetIterator.next();
-			if(!isPalindrome(current)){
+			if (!isPalindrome(current)) {
 				strSetIterator.remove();
 			}
 		}
 		System.out.println("Palindrome substrings ::: " + strSet);
 	}
 
-	public static Set<String> partitionString(String s){
+	/**
+	 * Partition s into set of all possible substrings
+	 * 
+	 * @param s
+	 *            : Input string
+	 * @return set of possible substrings of s
+	 */
+	public static Set<String> partitionString(String s) {
 		Set<String> strSet = new HashSet<>();
 		int length = s.length();
-		for(int l = 1; l <= length; l++){
-			for(int i = 0; i < l; i++){
+		for (int l = 1; l <= length; l++) {
+			for (int i = 0; i < l; i++) {
 				String subStr = s.substring(i, l);
 				strSet.add(subStr);
 			}
@@ -33,6 +46,13 @@ public class PalindromePartition {
 		return strSet;
 	}
 
+	/**
+	 * Checks whether s is a palindrome or not
+	 * 
+	 * @param s:
+	 *            input string
+	 * @return true if s is palindrome
+	 */
 	public static boolean isPalindrome(String s) {
 		int length = s.length();
 		if (length == 1) {

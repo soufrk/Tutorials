720185757c7e7eb095f461ab13dd785b3fda298b
saurabh.kedia <saurabh.kedia@MYSQLDB>
2016-10-26 19:13:43 +0530

    Jump Search and Generic Stack

---
+++ DataStructure And Algorithms/Data Structure and Algorithms/src/ds/stack/Stack.java
@@ -0,0 +1,40 @@
+package ds.stack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stack<T> {
+	private int top;
+	private List<T> dataList;
+	private int capacity;
+
+	public Stack() {
+		this.top = 0;
+		this.capacity = 16;
+		this.dataList = new LinkedList<>();
+	}
+
+	public void push(T data) throws Exception {
+		if (dataList.size() == capacity) {
+			throw new Exception("Stack is full");
+		}
+		dataList.add(data);
+		top++;
+	}
+
+	public T pop() throws Exception {
+		if (dataList.size() == 0) {
+			throw new Exception("Stack is empty");
+		}
+		T t = dataList.remove(top);
+		top--;
+		return t;
+	}
+
+	public T peek() throws Exception {
+		if (dataList.size() == 0) {
+			throw new Exception("Stack is empty");
+		}
+		return dataList.get(top);
+	}
+}
---
+++ DataStructure And Algorithms/Data Structure and Algorithms/src/search/algorithms/JumpSearch.java
@@ -0,0 +1,38 @@
+package search.algorithms;
+
+/**
+ * Optimal block size for jump search is square root of length of array. Jump
+ * the array by block size and find the index of element less than and more than
+ * the key. perform a linear search on it to find the key
+ * 
+ * @author saurabh.kedia
+ *
+ */
+public class JumpSearch {
+
+	public static void main(String[] args) {
+		int[] sortedIntArray = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 };
+		int key = 55;
+		int blockSize = (int) Math.floor(Math.sqrt(sortedIntArray.length));
+		int startIndex = -1;
+		int endIndex = -1;
+		for (int i = 0; i < sortedIntArray.length; i += blockSize) {
+			if (sortedIntArray[i] < key)
+				startIndex = i;
+			if (sortedIntArray[i] >= key)
+				endIndex = i;
+		}
+		if (startIndex == -1 || endIndex == -1) {
+			System.out.println("Key not found");
+		}
+
+		int position = -1;
+		for (int j = startIndex; j <= endIndex; j++) {
+			if (sortedIntArray[j] == key) {
+				position = j;
+			}
+		}
+		System.out.println((position != -1) ? "Key found at position " + position : "Key not found");
+	}
+
+}

f28c87fe1a28a4e0cee445ae229686f5ffe6df35
Souvik Goswami <soufrk@gmail.com>
2017-09-25 17:35:18 +0530

    Updated file
    Applied Java code format conventions.

--- CoreTutorials/src/learn/tutorial/threads/FileCounter.java
+++ CoreTutorials/src/learn/tutorial/threads/FileCounter.java
@@ -7,42 +7,42 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FileCounter {
 
-	public static Queue<String> fileNames = new ConcurrentLinkedQueue<>();
+    public static Queue<String> fileNames = new ConcurrentLinkedQueue<>();
 
-	public static void main(String[] args) throws InterruptedException {
-		String startDirPath = "D:\\threadDemo";
-		ThreadCounter tc = new ThreadCounter();
-		tc.dirPath = startDirPath;
-		tc.start();
-		tc.join();
-		System.out.println("Total No of files: " + fileNames.size() + " and the file names are:");
-		for (String s : fileNames)
-			System.out.println(s + "\t");
+    public static void main(String[] args) throws InterruptedException {
+	String startDirPath = "D:\\threadDemo";
+	ThreadCounter tc = new ThreadCounter();
+	tc.dirPath = startDirPath;
+	tc.start();
+	tc.join();
+	System.out.println("Total No of files: " + fileNames.size() + " and the file names are:");
+	for (String s : fileNames)
+	    System.out.println(s + "\t");
 
-	}
+    }
 
 }
 
 class ThreadCounter extends Thread {
 
-	public String dirPath = "";
+    public String dirPath = "";
 
-	@Override
-	public void run() {
-		File[] files = Paths.get(dirPath).toFile().listFiles();
-		for (File file : files) {
-			if (file.isDirectory()) {
-				ThreadCounter tc = new ThreadCounter();
-				tc.dirPath = file.getAbsolutePath();
-				tc.start();
-				try {
-					tc.join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			} else {
-				FileCounter.fileNames.add(file.getName());
-			}
+    @Override
+    public void run() {
+	File[] files = Paths.get(dirPath).toFile().listFiles();
+	for (File file : files) {
+	    if (file.isDirectory()) {
+		ThreadCounter tc = new ThreadCounter();
+		tc.dirPath = file.getAbsolutePath();
+		tc.start();
+		try {
+		    tc.join();
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
 		}
+	    } else {
+		FileCounter.fileNames.add(file.getName());
+	    }
 	}
+    }
 }
